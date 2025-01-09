79ec49ac904079207d346f7631789d0dcb6dd712
0xSmokey <p.roshanrajkamal@gmail.com>
2025-01-09 20:33:39 +0530

    Add: Static file accesss add in settings.py

--- skillshare/settings.py
+++ skillshare/settings.py
@@ -120,6 +120,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#This piece of code added to access the static files anywhere 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / "apps/home/static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
